081e8d81ca680711f4414dbdd927650cca66c2a5
GrayWandererr <ninaad@fulfillmentiq.com>
2025-04-29 16:03:39 -0400

    Update network dashboard component

--- components/network-dashboard.tsx
+++ components/network-dashboard.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
 
 export function NetworkDashboard() {
@@ -9,12 +8,11 @@ export function NetworkDashboard() {
       <Card className="flex-1">
         <CardContent className="p-0 h-full">
           <div className="relative w-full h-full min-h-[600px]">
-            <Image
-              src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-V8O7EjomQpDHDZ6FjQp7oMKSifHjHu.png"
-              alt="Network Level Executive Dashboard"
-              fill
-              className="object-contain"
-              priority
+            <iframe
+              title="Network Level Executive Dashboard"
+              src="https://app.powerbi.com/reportEmbed?reportId=1593b2d9-ae37-456e-8a94-d7963445aebc&autoAuth=true&ctid=0861cd9f-3d1e-46c3-b687-68e9b6ca794e"
+              className="w-full h-full border-0"
+              allowFullScreen
             />
           </div>
         </CardContent>
